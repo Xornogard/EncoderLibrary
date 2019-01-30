9acc0c5190036423a749272b1d4ccf007ee383f0
Xornogard <Miller.Daniel91@yahoo.co.uk>
2019-01-30 14:37:23 +0100

    Encoder works with INT0 interrupt

--- main.c
+++ main.c
@@ -6,14 +6,25 @@
  */ 
 
 #include <avr/io.h>
-
+#define F_CPU 16e6
+#include "encoderlib/encoder.h"
 
 int main(void)
 {
+	DDRB = 0xFF;
+
+	uint8_t led_position = 0;
+
+	enc_init();
+	
     /* Replace with your application code */
     while (1) 
     {
-
+		while (1)
+		{
+			led_position = enc_value%8;
+			PORTB = ~(1 << led_position);
+		}
     }
 }
 
