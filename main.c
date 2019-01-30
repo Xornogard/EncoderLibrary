2c6b5701d4e91b4cba6b507a8eae0eff0cc81a2e
Xornogard <Miller.Daniel91@yahoo.co.uk>
2019-01-30 14:04:06 +0100

    init commit

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+ * EncoderLibrary.c
+ *
+ * Created: 30.01.2019 13:57:38
+ * Author : Xornogard
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+
+    }
+}
+
